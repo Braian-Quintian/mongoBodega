e35476de112837e7c8e55c607fc650efcaf54e47
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 16:12:51 -0500

    feat: :sparkles: Se actualiza el código para las validaciones

--- controller/bodegas.js
+++ controller/bodegas.js
@@ -7,30 +7,39 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { Transform, Expose } from "class-transformer";
+import { Expose } from "class-transformer";
+import { IsNumber, IsString } from "class-validator";
 export class validateBodega {
-    constructor(p1, p2, p3) {
-        this.ID = p1;
-        this.NOM = p2;
-        this.AGE = p3;
+    constructor(p1, p2, p3, p4, p5) {
+        this.NOM = p1;
+        this.RESPONSABLE = p2;
+        this.STATE = p3;
+        this.CREADOR = p4;
+        this.ACTUALIZADOR = p5;
     }
 }
 __decorate([
-    Expose({ name: 'id' }),
-    Transform(({ value }) => {
-        let data = /^[0-9]+$/g.test(value);
-        if (data && typeof value == "number")
-            return Number(value);
-        else
-            throw { status: 401, message: "The value is not a number" };
-    }),
-    __metadata("design:type", Number)
-], validateBodega.prototype, "ID", void 0);
-__decorate([
     Expose({ name: "nombre" }),
+    IsString({ message: "Nombre must be a string" }),
     __metadata("design:type", String)
 ], validateBodega.prototype, "NOM", void 0);
 __decorate([
-    Expose({ name: "edad" }),
+    Expose({ name: "id_responsable" }),
+    IsNumber({}, { message: "ID Responsable must be a number" }),
     __metadata("design:type", Number)
-], validateBodega.prototype, "AGE", void 0);
+], validateBodega.prototype, "RESPONSABLE", void 0);
+__decorate([
+    Expose({ name: "estado" }),
+    IsNumber({}, { message: "Estado must be a number" }),
+    __metadata("design:type", Number)
+], validateBodega.prototype, "STATE", void 0);
+__decorate([
+    Expose({ name: "created_by" }),
+    IsNumber({}, { message: "Created By must be a number" }),
+    __metadata("design:type", Number)
+], validateBodega.prototype, "CREADOR", void 0);
+__decorate([
+    Expose({ name: "update_by" }),
+    IsNumber({}, { message: "Update By must be a number" }),
+    __metadata("design:type", Number)
+], validateBodega.prototype, "ACTUALIZADOR", void 0);
