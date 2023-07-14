91ba14815f9482086fcb7414fa371914db0a6ae0
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 14:26:35 -0500

    feat: :sparkles: Se crea el endpoint de Bodegas con metodo post}

--- src/middleware/bodegas.js
+++ src/middleware/bodegas.js
@@ -3,6 +3,7 @@ import { methodsBodegas } from "../routes/bodegas.routes.js";
 const router = Router();
 
 router.get('/', methodsBodegas.getBodegas);
+router.post('/', methodsBodegas.addBodegas);
 
 export {
     router
