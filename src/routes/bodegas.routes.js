6dc4bf90e66f4a59cf9bcca8d44a6abde2b4f0b0
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 16:12:29 -0500

    feat: :sparkles: Se añade el addBodegas para crear nuevas bodegas

--- src/routes/bodegas.routes.js
+++ src/routes/bodegas.routes.js
@@ -1,5 +1,8 @@
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+import { validateBodega } from '../../controller/bodegas.js';
 import { getConnection } from '../connection/conection.js';
-import { handleInternalServerError } from '../errors/errors.js';
+import { handleInternalServerError, handleDuplicateEntryError, handleInvalidDataError, handleNoExist } from '../errors/errors.js';
 
 const getBodegas = async (req, res) => {
     try {
@@ -12,6 +15,42 @@ const getBodegas = async (req, res) => {
     }
 };
 
+const addBodegas = async (req, res) => {
+    try {
+        const dataSend = plainToClass(validateBodega, req.body);
+        if (!dataSend.CREADOR) {
+            dataSend.CREADOR = dataSend.RESPONSABLE;
+        }
+        if (!dataSend.ACTUALIZADOR) {
+            dataSend.ACTUALIZADOR = dataSend.RESPONSABLE;
+        }
+
+        const validationErrors = await validate(dataSend);
+
+        if (validationErrors.length > 0) {
+            const errorMessages = validationErrors.map((error) => Object.values(error.constraints)).join(', ');
+            handleInvalidDataError(res, errorMessages);
+            return;
+        }
+
+        const connection = await getConnection();
+
+        const query = `INSERT INTO bodegas(nombre, id_responsable, estado, created_by, update_by) VALUES(?, ?, ?, ?, ?);`;
+        const values = [dataSend.NOM, dataSend.RESPONSABLE, dataSend.STATE, dataSend.CREADOR, dataSend.ACTUALIZADOR];
+        await connection.query(query, values);
+        res.json({ message: 'Bodega added successfully' });
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            handleDuplicateEntryError(res);
+        } else if (error.code === 'ER_NO_REFERENCED_ROW_2') {
+            handleNoExist(res);
+        } else {
+            handleInternalServerError(error, res);
+        }
+    }
+};
+ 
 export const methodsBodegas = {
-    getBodegas
-}
+    getBodegas,
+    addBodegas
+};
