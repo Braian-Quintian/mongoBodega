85c5f925c43e3f75f25adce14b4771ad89f105ca
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 17:09:26 -0500

    feat: :sparkles: Se agrega el router para productos

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { router as bodegasRouter } from '../src/middleware/bodegas.js'
+import { router as productosRouter } from '../src/middleware/productos.js'
 import { handleInternalServerError } from '../src/errors/errors.js';
 const app = express();
 app.use(express.json());
 
 app.use('/bodegas', bodegasRouter);
-
+app.use('/productos', productosRouter);
 
 app.use((err, res) => {
     handleInternalServerError(err, res);
