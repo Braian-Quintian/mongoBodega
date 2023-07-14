5badffb69a124a117461e4fb89115998c7e80d76
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 17:10:08 -0500

    feat: :sparkles: Se agrega el middleware para productos

---
+++ src/middleware/productos.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { methodsProductos } from "../routes/productos.routes.js";
+const router = Router();
+
+router.get('/', methodsProductos.getProductos);
+router.get('/total', methodsProductos.getTotalProductos);
+// router.post('/', methodsProductos.addProductos);
+
+export {
+    router
+}
