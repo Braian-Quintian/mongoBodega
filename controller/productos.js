3b7ba793989149f3c956b0527d1abcdc1fae86b2
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 22:35:21 -0500

    refactor: :art: Se actualiza el código

--- controller/productos.js
+++ controller/productos.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 import { Expose } from 'class-transformer';
 import { IsNumber, IsString } from 'class-validator';
-export class validateProductos {
+export class ValidateProductos {
     constructor(p1, p2, p3, p4, p5) {
         this.NOM = p1;
         this.DESCRIPCION = p2;
@@ -22,24 +22,24 @@ __decorate([
     Expose({ name: 'nombre' }),
     IsString({ message: 'Nombre must be a string' }),
     __metadata("design:type", String)
-], validateProductos.prototype, "NOM", void 0);
+], ValidateProductos.prototype, "NOM", void 0);
 __decorate([
     Expose({ name: 'descripcion' }),
     IsString({ message: 'Descripcion must be a string' }),
     __metadata("design:type", String)
-], validateProductos.prototype, "DESCRIPCION", void 0);
+], ValidateProductos.prototype, "DESCRIPCION", void 0);
 __decorate([
     Expose({ name: 'estado' }),
     IsNumber({}, { message: 'Estado must be a number' }),
     __metadata("design:type", Number)
-], validateProductos.prototype, "ESTADO", void 0);
+], ValidateProductos.prototype, "ESTADO", void 0);
 __decorate([
     Expose({ name: 'created_by' }),
     IsNumber({}, { message: 'Created By must be a number' }),
     __metadata("design:type", Number)
-], validateProductos.prototype, "CREADOR", void 0);
+], ValidateProductos.prototype, "CREADOR", void 0);
 __decorate([
     Expose({ name: 'update_by' }),
     IsNumber({}, { message: 'Update By must be a number' }),
     __metadata("design:type", Number)
-], validateProductos.prototype, "ACTUALIZADOR", void 0);
+], ValidateProductos.prototype, "ACTUALIZADOR", void 0);
