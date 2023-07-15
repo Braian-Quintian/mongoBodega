1f95a8e1055527d604282a05b22bee4fecec74d5
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 23:48:22 -0500

    feat: :sparkles: Se mejora la validación de la data

--- controller/productos.js
+++ controller/productos.js
@@ -7,7 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { IsNumber, IsString } from 'class-validator';
 export class ValidateProductos {
     constructor(p1, p2, p3, p4, p5) {
@@ -30,16 +30,19 @@ __decorate([
 ], ValidateProductos.prototype, "DESCRIPCION", void 0);
 __decorate([
     Expose({ name: 'estado' }),
+    Transform(({ value }) => parseInt(value)),
     IsNumber({}, { message: 'Estado must be a number' }),
     __metadata("design:type", Number)
 ], ValidateProductos.prototype, "ESTADO", void 0);
 __decorate([
     Expose({ name: 'created_by' }),
+    Transform(({ value }) => parseInt(value)),
     IsNumber({}, { message: 'Created By must be a number' }),
     __metadata("design:type", Number)
 ], ValidateProductos.prototype, "CREADOR", void 0);
 __decorate([
     Expose({ name: 'update_by' }),
+    Transform(({ value }) => parseInt(value)),
     IsNumber({}, { message: 'Update By must be a number' }),
     __metadata("design:type", Number)
 ], ValidateProductos.prototype, "ACTUALIZADOR", void 0);
