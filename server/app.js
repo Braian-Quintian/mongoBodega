cb7bb6fba9a952be218ad0262a2eb18cad426def
Braian-Quintian <bquintianromero@gmail.com>
2023-07-14 23:48:48 -0500

    feat: :sparkles: Se añade el endpoint para inventarios

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { router as bodegasRouter } from '../src/middleware/bodegas.js'
 import { router as productosRouter } from '../src/middleware/productos.js'
+import { router as inventariosRouter } from '../src/middleware/inventarios.js'
 import { handleInternalServerError } from '../src/errors/errors.js';
 const app = express();
 app.use(express.json());
 
 app.use('/bodegas', bodegasRouter);
 app.use('/productos', productosRouter);
+app.use('/inventarios', inventariosRouter);
 
 app.use((err, res) => {
     handleInternalServerError(err, res);
