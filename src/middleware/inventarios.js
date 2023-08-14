4c5c5b06c3420340229fa5b755aaddb6c113ff48
Braian-Quintian <bquintianromero@gmail.com>
2023-08-14 02:01:07 -0500

    feat: :sparkles: Se añade el limit

--- src/middleware/inventarios.js
+++ src/middleware/inventarios.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { methodsInventarios } from "../routes/inventarios.routes.js";
+import {limitPost} from '../rules/reglas.js';
 const router = Router();
 
-router.post('/', methodsInventarios.addInventarios);
+router.post('/', limitPost(),methodsInventarios.addInventarios);
 
 export {
     router
--- src/middleware/productos.js
+++ src/middleware/productos.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { methodsProductos } from "../routes/productos.routes.js";
+import {limitGet, limitPost} from '../rules/reglas.js';
 const router = Router();
 
-router.get('/', methodsProductos.getProductos);
-router.get('/total', methodsProductos.getTotalProductos);
-router.post('/', methodsProductos.addProductos);
+router.get('/', limitGet(),methodsProductos.getProductos);
+// router.get('/total', limitGet(),methodsProductos.getTotalProductos);
+// router.post('/',limitPost() ,methodsProductos.addProductos);
 
 export {
     router
