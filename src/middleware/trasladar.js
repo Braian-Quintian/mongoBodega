560aee7cbbd60fbe8b95cb2ef14d3dafc73a195d
Braian-Quintian <bquintianromero@gmail.com>
2023-08-14 02:03:26 -0500

    feat: :hammer: Se añade a al archivo gitignore la carpeta controller que ya no se usa

--- src/middleware/trasladar.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { methodsTraslados } from "../routes/trasladar.routes.js";
-const router = Router();
-
-router.post('/', methodsTraslados.addTraslados);
-
-export {
-    router
-}
