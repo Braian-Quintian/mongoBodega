ee9ff7a939a5574b0e8948e311a0d7abc7679b0f
Braian-Quintian <bquintianromero@gmail.com>
2023-08-14 01:30:07 -0500

    feat: :sparkles: Se le añaden los limit() a los endpoints de la collection bodega

--- src/middleware/bodegas.js
+++ src/middleware/bodegas.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { methodsBodegas } from "../routes/bodegas.routes.js";
+import {limitGet, limitPost} from '../rules/reglas.js';
 const router = Router();
 
-router.get('/', methodsBodegas.getBodegas);
-router.post('/', methodsBodegas.addBodegas);
+router.get('/', limitGet(),methodsBodegas.getBodegas);
+router.post('/', limitPost(), methodsBodegas.addBodegas);
 
 export {
     router
