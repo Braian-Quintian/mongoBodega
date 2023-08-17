31c72f145d59bfd943fc83eec7ab36bbe88f18f6
Braian-Quintian <bquintianromero@gmail.com>
2023-08-17 14:12:10 -0500

    feat: :sparkles: Se actualiza el typescript

--- src/typescript/productos.ts
+++ src/typescript/productos.ts
@@ -1,5 +1,5 @@
 import { Expose } from 'class-transformer';
-import { IsString, IsDefined, IsNumber,IsDate, MaxLength,IsOptional,IsNotEmpty,ValidateIf } from 'class-validator';
+import { IsString, IsDefined, IsNumber,IsDate, MaxLength,IsOptional,IsNotEmpty,ValidateIf,Max } from 'class-validator';
 
 export class ProductosG {
     @Expose({ name: "id"})
@@ -100,4 +100,61 @@ export class ProductosGT {
 
 export class ProductosP {
     
+    @Expose({name: "id-producto"})
+    @IsNotEmpty({ message: () => { throw { status: 422, message: "The id-producto cannot be empty" } } })
+    @IsDefined({ message: () => { throw { status: 422, message: "The id-producto is required" } } })
+    @IsNumber({}, { message: () => { throw { status: 406, message: "The id-producto must be a number" } } })
+    @Max(11, { message: () => { throw { status: 406, message: "The id-producto cannot contain more than 11 characters"}}})
+    ID_Producto: number
+
+    @Expose({name: 'nombre-producto'})
+    @IsNotEmpty({ message: () => { throw { status: 422, message: "The nombre-producto cannot be empty" } } })
+    @IsDefined({ message: () => { throw { status: 422, message: "The nombre-producto is required" } } })
+    @MaxLength(25, { message: () => { throw { status: 406, message: "The nombre-producto cannot contain more than 25 characters"}}})
+    @IsString({ message: () => { throw { status: 406, message: "The nombre-producto must be a string" } } })
+    Nombre: string;
+
+    @Expose({name: "descripcion-producto"})
+    @IsNotEmpty({ message: () => { throw { status: 422, message: "The descripcion-producto cannot be empty" } } })
+    @IsDefined({ message: () => { throw { status: 422, message: "The descripcion-producto is required" } } })
+    @MaxLength(100, { message: () => { throw { status: 406, message: "The descripcion-producto cannot contain more than 100 characters"}}})
+    @IsString( { message: () => { throw { status: 406, message: "The descripcion-producto must be a string" } } })
+    Description: string
+
+    @Expose({name: "estado-producto"})
+    @IsNotEmpty({ message: () => { throw { status: 422, message: "The estado-producto cannot be empty" } } })
+    @IsDefined({ message: () => { throw { status: 422, message: "The estado-producto is required" } } })
+    @Max(1, { message: () => { throw { status: 406, message: "The estado-producto cannot contain more than 1 characters"}}})
+    @IsNumber({}, { message: () => { throw { status: 406, message: "The estado-producto must be a number" } } })
+    Estado: number
+
+    @Expose({name: "created-by"})
+    @IsNotEmpty({ message: () => { throw { status: 422, message: "The created-by cannot be empty" } } })
+    @IsDefined({ message: () => { throw { status: 422, message: "The created-by is required" } } })
+    @Max(20, { message: () => { throw { status: 406, message: "The created-by cannot contain more than 20 characters"}}})
+    @IsNumber({}, { message: () => { throw { status: 406, message: "The created-by must be a number" } } })
+    Creador: number
+
+    @Expose({name: "update-by"})
+    @IsOptional()
+    @Max(20, { message: () => { throw { status: 406, message: "The update-by cannot contain more than 20 characters"}}})
+    @IsNumber({}, { message: () => { throw { status: 406, message: "The update-by must be a number" } } })
+    Update: number
+
+    @Expose({name: "created-at"})
+    @IsOptional()
+    @IsDate({ message: () => { throw { status: 406, message: "The created-at must be a date" } } })
+    CreatedAt: string
+
+    @Expose({name: "updated-at"})
+    @IsOptional()
+    @IsDate({ message: () => { throw { status: 406, message: "The updated-at must be a date" } } })
+    UpdatedAt: string
+
+    @Expose({name: "deleted-at"})
+    @IsOptional()
+    @IsDate({ message: () => { throw { status: 406, message: "The deleted-at must be a date" } } })
+    DeletedAt: string
+
+    constructor(data: Partial<ProductosP>) {Object.assign(this, data)}
 }
